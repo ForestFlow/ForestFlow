574b8e67837406785e95dda04ae16a764d7f8ddc
aalkilani <ahmad.alkilani@dreamworks.com>
2019-11-05 14:42:47 -0800

    first public commit with code and docs
    Signed-off-by: Ahmad Alkilani <amkcom@gmail.com>

---
+++ common/src/main/scala/com/dreamworks/forestflow/scalapb/SealedOneofSupport.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2019 DreamWorks Animation L.L.C.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dreamworks.forestflow.scalapb
+
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
+import akka.http.scaladsl.model.{ContentTypeRange, MediaRanges, MediaType}
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
+import scalapb.GeneratedSealedOneof
+
+import scala.reflect.ClassTag
+
+// application/octet-stream
+trait SealedOneofSupport {
+  private val applicableMediaTypes: List[ContentTypeRange] = List(
+    MediaRanges.`*/*`,
+    MediaType.applicationBinary("octet-stream", MediaType.NotCompressible)
+  )
+
+  implicit def GeneratedSealedOneofBinaryUnmarshaller[T <: GeneratedSealedOneof]: FromEntityUnmarshaller[GeneratedSealedOneof]  = {
+    Unmarshaller.byteArrayUnmarshaller.forContentTypes(applicableMediaTypes: _*).map(b =>  b.asInstanceOf[GeneratedSealedOneof].asMessage.companion.parseFrom(b).asInstanceOf[T])
+  }
+
+  implicit def GeneratedSealedOneofBinaryMarshaller[T <: GeneratedSealedOneof](implicit tag: ClassTag[T]): ToEntityMarshaller[T]  = {
+    val result = Marshaller.ByteArrayMarshaller.compose((c: T) => c.asMessage.toByteArray)
+    result
+  }
+}
---
+++ core/src/main/scala/com/dreamworks/forestflow/serving/interfaces/ServeRequest.scala
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2019 DreamWorks Animation L.L.C.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dreamworks.forestflow.serving.interfaces
+
+import com.dreamworks.forestflow.domain._
+import com.dreamworks.forestflow.utils.SourceStorageProtocols
+import com.dreamworks.forestflow.utils.SourceStorageProtocols.SupportsFQRVExtraction
+
+
+trait ServeRequest extends Product {
+  this: ServeRequestShim => // if you implement ServeRequest then you must extend ServeRequestShim as well (gotta think about this)
+  def path: String
+
+  protected[this] def protocolOpt = SourceStorageProtocols.getProtocolOption(path)
+
+  def fqrv: Option[FQRV]
+
+  def servableSettings: ServableSettings
+
+  val contractSettings : Option[ContractSettings]
+
+  def withServableSettings(servableSettings: ServableSettings): ServeRequestShim
+  def withContractSettings(contractSettings: ContractSettings): ServeRequestShim
+
+  /**
+    * Use to resolve an FQRV based on protocol path or supplied fqrv.proto
+    * Supplied fqrv.proto is guarded for correctness (availability) by pathRequirements and require statements
+    *
+    * @return
+    */
+  def getUltimateFQRV: FQRV = {
+    protocolOpt match {
+      case Some(p: SupportsFQRVExtraction) =>
+        fqrv.getOrElse(p.getFQRV(path).get)
+      case _ =>
+        fqrv.get
+    }
+  }
+
+  def checkRequirements(): Unit = {
+    def pathRequirement = {
+      protocolOpt match {
+        case None => (false, Some(s"path provided doesn't follow a supported protocol: $path"))
+        case Some(protocol: SupportsFQRVExtraction) if protocol.hasValidFQRV(path) =>
+          (true, None) // s"App: Received request for protocol with SupportsFQRVExtraction"
+        case Some(p: SourceStorageProtocols.EnumVal) if fqrv.isEmpty => (false, Some(s"FQRV (Fully Qualified Release Version) is required with protocols that don't have implicit FQRV extraction support or where path doesn't follow FQRV extraction requirements"))
+        case Some(p: SourceStorageProtocols.EnumVal) => (true, None)
+      }
+    }
+
+    val (pathValid, msg) = pathRequirement
+    require(pathValid, msg.getOrElse("Invalid path"))
+    require(
+      servableSettings.loggingSettings.isDefined &&
+        !(servableSettings.loggingSettings.get.logLevel != LogLevel.NONE &&
+          servableSettings.loggingSettings.get.keyFeatures.isEmpty
+          ),
+      "LogLevel cannot be specified without defining set of features that define the key for logged messages")
+  }
+}
+
+/*
+trait MLFlowModel {
+  this: ServeRequest =>
+}
+
+trait BasicModel {
+  this: ServeRequest =>
+}*/
