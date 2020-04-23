98666462a686f981dcf1d25220de2fbb4589d7fb
Ahmad Alkilani <amkcom@gmail.com>
2020-04-22 17:43:21 -0700

    reamed buildah to build
    Signed-off-by: Ahmad Alkilani <amkcom@gmail.com>

--- buildah/src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-exec java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false ${JVM_OPTS} -jar ${FORESTFLOW_BIN}/${FORESTFLOW_FILENAME}
