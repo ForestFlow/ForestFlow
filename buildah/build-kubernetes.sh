62e0008198be4f88cf12f703ce6eaa7020faeb81
Ahmad Alkilani <amkcom@gmail.com>
2020-04-22 17:43:21 -0700

    reamed buildah to build
    Signed-off-by: Ahmad Alkilani <amkcom@gmail.com>

--- buildah/build-kubernetes.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Set additional local variables
-builddir=$(dirname "${BASH_SOURCE[0]}")
-source ${builddir}/vars
-
-mvn clean package -f ${builddir}/../pom.xml -P K8s -Dforestflow-latest.version=$FORESTFLOW_VERSION
